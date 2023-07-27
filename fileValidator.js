2b0f517d0ea45f091e935443d388e9e10a0a1fd1
suryakant <suryakant2125@outlook.com>
2023-07-28 00:14:12 +0530

    internal code updated

--- fileValidator.js
+++ fileValidator.js
@@ -7,69 +7,150 @@ module.exports = fileValidator = (args, outputFunction) => {
     returnBase64 = true,
   } = args || {};
 
-  // all type of error handling start...
-  if (!file) {
-    return { type: "error", message: "file is undefined" };
-  }
-  if (!Array.isArray(media)) {
-    return { type: "error", message: "media should be array of srting" };
-  }
-  if (!Array.isArray(whitelistExtension)) {
-    return {
-      type: "error",
-      message: "whitelistExtension should be array of srting",
-    };
-  }
-  if (!Array.isArray(blacklistExtension)) {
-    return {
-      type: "error",
-      message: "blacklistExtension should be array of srting",
-    };
-  }
-  // all type of error handling end...
-
   let checkFileAgainst = []; // file will be validated against these extension
 
-  // set default extension for all media type start...
-  if (media.includes("image")) {
-    checkFileAgainst = [
-      ...checkFileAgainst,
-      "jpg",
-      "jpeg",
-      "png",
-      "gif",
-      "webp",
-      "tiff",
-      "bmp",
-    ];
+  const getBase64 = (file) => {
+    // converting file to base64
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const returnSuccessResult = () => {
+    // return success result to user
+    if (returnBase64) {
+      getBase64(file).then((returnedBase64) =>
+        outputFunction({
+          type: "success",
+          file: file,
+          base64: returnedBase64,
+          fileChedkedAgaistExtension: checkFileAgainst,
+        })
+      );
+    } else {
+      outputFunction({
+        type: "success",
+        file: file,
+        fileChedkedAgaistExtension: checkFileAgainst,
+      });
+    }
+  };
+
+  const returnErrorResult = (message) => {
+    // return error result to user
+    outputFunction({
+      type: "error",
+      message: message,
+      fileChedkedAgaistExtension: checkFileAgainst,
+    });
+  };
+
+  // all props error handling start...
+  if (!file) {
+    return returnErrorResult("file not provided");
   }
-  if (media.includes("audio")) {
-    checkFileAgainst = [
-      ...checkFileAgainst,
-      "mp3",
-      "ogg",
-      "wav",
-      "mid",
-      "midi",
-    ];
+  if (!Array.isArray(media)) {
+    return returnErrorResult("media should be array of srting");
   }
-  if (media.includes("application")) {
-    checkFileAgainst = [
-      ...checkFileAgainst,
+  if (!Array.isArray(whitelistExtension)) {
+    return returnErrorResult("whitelistExtension should be array of srting");
+  }
+  if (!Array.isArray(blacklistExtension)) {
+    return returnErrorResult("blacklistExtension should be array of srting");
+  }
+  // all props error handling end...
+
+  const mediaExtensions = {
+    // media and supported extensions
+    image: ["jpg", "jpeg", "png", "gif", "webp", "tiff", "bmp"],
+    audio: ["mp3", "ogg", "wav", "mid", "midi"],
+    application: [
       "doc",
       "xls",
       "ppt",
       "docx",
+      "xlsx",
+      "pptx",
       "zip",
       "apk",
-      "pptx",
-      "xlsx",
       "pdf",
-    ];
-  }
-  // set default extension for all media type end...
+    ],
+  };
 
-  if (whitelistExtension.length > 0) {
+  const extensionSignature = {
+    // extensions and file singnature
+    // image extensions
+    jpg: [
+      "ffd8ffe0",
+      "ffd8ffe1",
+      "ffd8ffe2",
+      "ffd8ffe3",
+      "ffd8ffe8",
+      "ffd8ffee",
+      "69660000",
+      "49460001",
+    ],
+    jpeg: [
+      "ffd8ffe0",
+      "ffd8ffe1",
+      "ffd8ffe2",
+      "ffd8ffe3",
+      "ffd8ffe8",
+      "ffd8ffee",
+      "69660000",
+      "49460001",
+    ],
+    png: ["89504e47"],
+    gif: ["47494638"],
+    webp: ["52494646", "57454250"],
+    tiff: ["49492a00", "4d4d002a"],
+    bmp: ["424d"],
+
+    // audio extensions
+    mp3: ["494433"],
+    ogg: ["4f676753"],
+    wav: ["52494646", "57415645"],
+    mid: ["4d546864"],
+    midi: ["4d546864"],
+
+    //application extensions
+    doc: ["d0cf11e0a1b11ae1"],
+    xls: ["d0cf11e0a1b11ae1"],
+    ppt: ["d0cf11e0a1b11ae1"],
+    docx: ["504b0304", "504b0506", "504b0708"],
+    xlsx: ["504b0304", "504b0506", "504b0708"],
+    pptx: ["504b0304", "504b0506", "504b0708"],
+    zip: ["504b0304", "504b0506", "504b0708"],
+    apk: ["504b0304", "504b0506", "504b0708"],
+    pdf: ["255044462d"],
+  };
+
+  // setting list of extension, file will be checked start...
+
+  if (media.length) {
+    // setting default extensions for media to be checked
+    let requiredMediaDefaultExtension = [];
+
+    for (let i = 0; i < media.length; i++) {
+      if (mediaExtensions[media[i].toLowerCase()]) {
+        requiredMediaDefaultExtension = [
+          ...requiredMediaDefaultExtension,
+          ...mediaExtensions[media[i].toLowerCase()],
+        ];
+      } else {
+        returnErrorResult("media not supported");
+        break;
+      }
+    }
+    checkFileAgainst = [...requiredMediaDefaultExtension];
+  }
+
+  if (whitelistExtension.length) {
     // updating checkFileAgainst for white listed extension
     let whitelistExtensionNotFoundIncheckFileAgainst =
       whitelistExtension.filter(
@@ -81,7 +162,8 @@ module.exports = fileValidator = (args, outputFunction) => {
       ...whitelistExtensionNotFoundIncheckFileAgainst,
     ];
   }
-  if (blacklistExtension.length > 0) {
+
+  if (blacklistExtension.length) {
     // updating checkFileAgainst for black listed extension
     let removeBlacklistExtension = checkFileAgainst.filter(
       (eachCheckFileAgainst) =>
@@ -111,6 +193,10 @@ module.exports = fileValidator = (args, outputFunction) => {
     checkFileAgainst = [...removeBlacklistExtension];
   }
 
+  // setting list of extension, file will be checked end...
+
+  // validation code start...
+
   let blob = file;
   let fileReader = new FileReader(file);
   fileReader.onloadend = (event) => findValidation(event.target.result);
@@ -125,130 +211,27 @@ module.exports = fileValidator = (args, outputFunction) => {
       mimeString += array[i].toString(16);
     }
 
-    // validation code start...
-    // all image extionsios...
-    if (
-      (mimeString.includes("ffd8ffe0") ||
-        mimeString.includes("ffd8ffe1") ||
-        mimeString.includes("ffd8ffe2") ||
-        mimeString.includes("ffd8ffe3") ||
-        mimeString.includes("ffd8ffe8") ||
-        mimeString.includes("ffd8ffee")) &&
-      (checkFileAgainst.includes("jpg") || checkFileAgainst.includes("jpeg"))
-    ) {
-      returnSuccessResult();
-    } else if (
-      mimeString.includes("89504e47") &&
-      checkFileAgainst.includes("png")
-    ) {
-      returnSuccessResult();
-    } else if (
-      mimeString.includes("47494638") &&
-      checkFileAgainst.includes("gif")
-    ) {
-      returnSuccessResult();
-    } else if (
-      (mimeString.includes("52494646") || mimeString.includes("57454250")) &&
-      checkFileAgainst.includes("webp")
-    ) {
-      returnSuccessResult();
-    } else if (
-      (mimeString.includes("49492a00") || mimeString.includes("4d4d002a")) &&
-      checkFileAgainst.includes("tiff")
-    ) {
-      returnSuccessResult();
-    } else if (
-      mimeString.includes("424d") &&
-      checkFileAgainst.includes("bmp")
-    ) {
-      returnSuccessResult();
-    }
-    // all audio extensions...
-    else if (
-      mimeString.includes("494433") &&
-      checkFileAgainst.includes("mp3")
-    ) {
-      returnSuccessResult();
-    } else if (
-      mimeString.includes("4f676753") &&
-      checkFileAgainst.includes("ogg")
-    ) {
-      returnSuccessResult();
-    } else if (
-      (mimeString.includes("52494646") || mimeString.includes("57415645")) &&
-      checkFileAgainst.includes("wav")
-    ) {
-      returnSuccessResult();
-    } else if (
-      mimeString.includes("4d546864") &&
-      (checkFileAgainst.includes("mid") || checkFileAgainst.includes("midi"))
-    ) {
-      returnSuccessResult();
-    }
-    // all application extensions...
-    else if (
-      mimeString.includes("d0cf11e0a1b11ae1") &&
-      (checkFileAgainst.includes("doc") ||
-        checkFileAgainst.includes("xls") ||
-        checkFileAgainst.includes("ppt"))
-    ) {
-      returnSuccessResult();
-    } else if (
-      (mimeString.includes("504b0304") ||
-        mimeString.includes("504b0506") ||
-        mimeString.includes("504b0708")) &&
-      (checkFileAgainst.includes("docx") ||
-        checkFileAgainst.includes("zip") ||
-        checkFileAgainst.includes("apk") ||
-        checkFileAgainst.includes("pptx") ||
-        checkFileAgainst.includes("xlsx"))
-    ) {
-      returnSuccessResult();
-    } else if (
-      mimeString.includes("255044462d") &&
-      (checkFileAgainst.includes("pdf"))
-    ) {
-      returnSuccessResult();
-    } else {
-      returnErrorResult("file not supported");
-    }
-  };
+    let resultFound = false;
 
-  const returnSuccessResult = () => {
-    // return success result to user
-    if (returnBase64) {
-      getBase64(file).then((returnedBase64) =>
-        outputFunction({
-          type: "success",
-          file: file,
-          base64: returnedBase64,
-        })
-      );
-    } else {
-      outputFunction({
-        type: "success",
-        file: file,
-      });
+    for (let i = 0; i < checkFileAgainst.length; i++) {
+      let fileSignatureList = extensionSignature[checkFileAgainst[i]];
+
+      if (!resultFound) {
+        for (let j = 0; j < fileSignatureList.length; j++) {
+          if (mimeString.includes(fileSignatureList[j])) {
+            returnSuccessResult();
+            resultFound = true;
+            return;
+          }
+        }
+        if (i === checkFileAgainst.length - 1) {
+          returnErrorResult("no match found");
+          return;
+        }
+      } else {
+        return;
+      }
     }
-  };
-
-  const returnErrorResult = (message) => {
-    // return error result to user
-    outputFunction({
-      type: "error",
-      message: message,
-    });
-  };
-
-  const getBase64 = (file) => {
-    // converting file to base64
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = (error) => {
-        reject(error);
-      };
-    });
+    // validation code end...
   };
 };
